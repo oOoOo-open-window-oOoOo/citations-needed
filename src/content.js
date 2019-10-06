a043ac639a094e9174ffb8ac23eabf845af2eb59
simulcast <thisissimulcast@gmail.com>
2019-10-06 16:43:32 -0700

    changed content structure
    Co-authored-by: Will Urmston <wurmston@risd.edu>

--- src/content.js
+++ src/content.js
@@ -25,10 +25,114 @@ export default {
       id: 0,
       soundcloudId: 1234,
       runtime: "1 hour, 7 minutes",
-      datePosted: "December 21, 2019",
+      datePosted: "2019-12-21T08:00:00.000Z",
       title: 'Episode 84: How Claims of “Sowing Discord” Silence Criticism of Power',
       coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
-      contentWarning: 'Violence',
+      contentWarning: 'violence',
+      description: `
+        Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
+            
+        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves. 
+            
+        We are told again and again that progressives criticizing party leaders is helping Trump. That fighting Trump’s racism is merely “playing into his hands,” that we shouldn’t attack other democrats in the primary too harshly lest it “give us four more years of Trump.”
+            
+        But there’s a major problem with this: There’s no evidence that intra-party fighting loses elections or assists the "other side." In many ways, it may actually help engage voters and make them feel heard, rather than viewed as box-checkers for the already anointed.
+        We are joined by Maximillian Alvarez of the podcast Working People.
+      `,
+      content: [
+        { // IMAGE
+          id: 321,
+          type: 'image',
+          url: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 123012
+        },
+        { // TEXT
+          id: 321,
+          type: 'text',
+          url: 'https://image.com/assets/aoc.jpg',
+          text: `
+            ## Freshman Congresswoman Alexandria Ocasio-Cortez
+            - some more text
+            - a list
+          `,
+          startTime: 123012
+        },
+        { // WEB CLIPPING
+          id: 321,
+          type: 'embed',
+          html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/p78q5T2fkcY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 122459
+        }
+      ],
+      credits: `
+        ...
+      `,
+      transcript: `
+        ...
+      `
+    },
+    { // EPISODE
+      id: 0,
+      soundcloudId: 1234,
+      runtime: "1 hour, 7 minutes",
+      datePosted: "2019-12-18T08:00:00.000Z",
+      title: 'Episode 83: Blah blah',
+      coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+      contentWarning: 'violence',
+      description: `
+        Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
+            
+        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves. 
+            
+        We are told again and again that progressives criticizing party leaders is helping Trump. That fighting Trump’s racism is merely “playing into his hands,” that we shouldn’t attack other democrats in the primary too harshly lest it “give us four more years of Trump.”
+            
+        But there’s a major problem with this: There’s no evidence that intra-party fighting loses elections or assists the "other side." In many ways, it may actually help engage voters and make them feel heard, rather than viewed as box-checkers for the already anointed.
+        We are joined by Maximillian Alvarez of the podcast Working People.
+      `,
+      content: [
+        { // IMAGE
+          id: 321,
+          type: 'image',
+          url: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 123012
+        },
+        { // TEXT
+          id: 321,
+          type: 'text',
+          url: 'https://image.com/assets/aoc.jpg',
+          text: `
+            ## Freshman Congresswoman Alexandria Ocasio-Cortez
+            - some more text
+            - a list
+          `,
+          startTime: 123012
+        },
+        { // WEB CLIPPING
+          id: 321,
+          type: 'embed',
+          html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/p78q5T2fkcY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 122459
+        }
+      ],
+      credits: `
+        ...
+      `,
+      transcript: `
+        ...
+      `
+    },
+    { // EPISODE
+      id: 0,
+      soundcloudId: 1234,
+      runtime: "1 hour, 7 minutes",
+      datePosted: "2019-12-11T08:00:00.000Z",
+      title: 'Episode 82: Blah blah',
+      coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+      contentWarning: 'violence',
       description: `
         Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
             
