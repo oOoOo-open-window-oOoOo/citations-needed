0335c089cd9eb8bd900dc484b27cb9602661a10c
Coby Tamayo <acobster@gmail.com>
2019-11-03 08:42:38 -0800

    slightly more realistic content

--- src/content.js
+++ src/content.js
@@ -35,11 +35,11 @@ export default {
       contentWarning: 'violence',
       description: `
         Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
-            
-        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves. 
-            
+
+        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves.
+
         We are told again and again that progressives criticizing party leaders is helping Trump. That fighting Trump’s racism is merely “playing into his hands,” that we shouldn’t attack other democrats in the primary too harshly lest it “give us four more years of Trump.”
-            
+
         But there’s a major problem with this: There’s no evidence that intra-party fighting loses elections or assists the "other side." In many ways, it may actually help engage voters and make them feel heard, rather than viewed as box-checkers for the already anointed.
         We are joined by Maximillian Alvarez of the podcast Working People.
       `,
@@ -83,18 +83,17 @@ export default {
       runtime: "1 hour, 7 minutes",
       duration: 360000,
       datePosted: "2019-12-18T08:00:00.000Z",
-      title: 'Episode 83: Blah blah',
+      title: 'Episode 83: The Unchecked Conservative Ideology of US Media\'s \'Fact-Check\' Verticals',
       coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
       contentWarning: 'violence',
       description: `
-        Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
-            
-        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves. 
-            
-        We are told again and again that progressives criticizing party leaders is helping Trump. That fighting Trump’s racism is merely “playing into his hands,” that we shouldn’t attack other democrats in the primary too harshly lest it “give us four more years of Trump.”
-            
-        But there’s a major problem with this: There’s no evidence that intra-party fighting loses elections or assists the "other side." In many ways, it may actually help engage voters and make them feel heard, rather than viewed as box-checkers for the already anointed.
-        We are joined by Maximillian Alvarez of the podcast Working People.
+        "Three Pinocchios!" rates The Washington Post. "Pants On Fire!" declares PolitiFact. “True, but misleading,” assess The New York Times.
+
+        In a media environment overwhelmed with information, misinformation, disinformation and so-called “fake news,” a cottage industry has emerged to “fact-check” the content coming across our screens. Prestige, corporate media outlets tell us if a viral meme, a politician’s statement or a pundit's controversial claims is indeed “factually correct.”
+
+        But who fact-checks the fact-checkers? And what do mainstream media’s particular hyper-literal, decontextualized approach to “facts” and “truth” say about how the press views its role as ideological gate keeper?
+
+        We are joined by writer Andrew Hart.
       `,
       content: [
         { // IMAGE
@@ -136,18 +135,122 @@ export default {
       runtime: "1 hour, 7 minutes",
       duration: 360000,
       datePosted: "2019-12-11T08:00:00.000Z",
-      title: 'Episode 82: Blah blah',
+      title: 'Episode 81: How US Media Pits Labor and Climate Activists Against One Other',
       coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
       contentWarning: 'violence',
       description: `
-        Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi, we are told, “plays into the hands of Trump.” Russians are using Black Lives Matter and anti-fracking activists to “sow discord,” insists CNN. We must “be united” rather than “divided.”
-            
-        Everywhere we turn we are told by high-status pundits that we shouldn’t air our criticisms of power at this particular moment with any reasonable degree of severity lest our mutual enemies exploit these divisions to empower themselves. 
-            
-        We are told again and again that progressives criticizing party leaders is helping Trump. That fighting Trump’s racism is merely “playing into his hands,” that we shouldn’t attack other democrats in the primary too harshly lest it “give us four more years of Trump.”
-            
-        But there’s a major problem with this: There’s no evidence that intra-party fighting loses elections or assists the "other side." In many ways, it may actually help engage voters and make them feel heard, rather than viewed as box-checkers for the already anointed.
-        We are joined by Maximillian Alvarez of the podcast Working People.
+        "A growing, and likely irreparable, rift between elite progressive environmentalists," Forbes tells us. "Environmentalists need to reconnect with blue-collar America," The Hill explains. "Labor anger over Green New Deal greets 2020 contenders in California," Politico reports. "AOC's Green New Deal could have Dems facing blue-collar backlash at polls, some say," a Fox News headline reads.
+
+        One of the few times corporate media cares what "American labor" has to say is when they’re using them as wedge against other elements of the Left, namely environmentalists and activists calling for urgent solutions to climate change. The narrative they’re reinforcing: a broadly assumed––but largely baseless––premise that climate change is a boutique issue for wealthy liberals that real working people don’t care about.
+
+        For a media that still largely views the working class as a white-man-with-a-hard-hat caricature, this fits into a nice binary that undermines both efforts to take on fossil fuel companies and improve the lives of workers. But who does the false dichotomy serve? How does the media highlight and misconstrue real points of tension to undermine both groups, and what can activists do to resolve good faith differences without playing into power-serving “hardhats vs. hippies” cliches?
+        And what do we mean when we say “labor”? How do workers drowning in the South Pacific or displaced in South Sudan factor into our notion of what’s at stake in the "labor vs. environmentalist" debate about climate change?
+
+        We are joined on this episode by writer and editor Michelle Chen.
+      `,
+      content: [
+        { // IMAGE
+          id: 321,
+          type: 'image',
+          url: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 123012
+        },
+        { // TEXT
+          id: 321,
+          type: 'text',
+          url: 'https://image.com/assets/aoc.jpg',
+          text: `
+            ## Freshman Congresswoman Alexandria Ocasio-Cortez
+            - some more text
+            - a list
+          `,
+          startTime: 123012
+        },
+        { // WEB CLIPPING
+          id: 321,
+          type: 'embed',
+          html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/p78q5T2fkcY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 122459
+        }
+      ],
+      credits: `
+        ...
+      `,
+      transcript: `
+        ...
+      `
+    },
+    { // EPISODE
+      id: 0,
+      soundcloudId: 1234,
+      runtime: "1 hour, 7 minutes",
+      duration: 360000,
+      datePosted: "2019-12-11T08:00:00.000Z",
+      title: 'Episode 82: \'Western Civilization\' and White Supremacy: The Right-Wing Co-option of Antiquity',
+      coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+      contentWarning: 'violence',
+      description: `
+        The term "Western civilization" has long been a staple of the American Right, but with the recent resurgence of white nationalism, it is having something of a comeback. Glenn Beck and Bill O'Reilly are hosting a two-week Mediterranean "cruise thru history" to "explore the roots of Western civilization." The Intellectual Dark Web's Jordan Peterson tells us “The West is Right,” while The Daily Caller and Fox News are busy “celebrating the West." Neo-Nazi Matthew Heimbach hails “Youth for Western Civilization." Both the traditional and so-called alt-right ground their worldviews in a fictional moral arc of "The West” that bares little resemblance to reality.
+
+        Learning from the past and applying those lessons to the present is a good thing. But in pop political discourse, the Classics have been misused and abused to promote an origin story that never was - a white Greco-Roman world birthing our noble, so-called “Judeo-Christian” American empire to gloss over a history of exploitation, imperialism, slavery and conquest.
+
+        On this episode, we’ll explore the right-wing obsession with the ancient world, it’s influence on neoconservative empire-building and alt-right white nationalism alike, and how our common cultural understanding of the ancient world has been perpetually white-washed to promote a clash of civilizations narrative and racist pseudo-science.
+
+        We are joined by Dr. Sarah E. Bond, Associate Professor at the University of Iowa, and Dr. Cord Whitaker, Associate Professor at Wellesley College.
+      `,
+      content: [
+        { // IMAGE
+          id: 321,
+          type: 'image',
+          url: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 123012
+        },
+        { // TEXT
+          id: 321,
+          type: 'text',
+          url: 'https://image.com/assets/aoc.jpg',
+          text: `
+            ## Freshman Congresswoman Alexandria Ocasio-Cortez
+            - some more text
+            - a list
+          `,
+          startTime: 123012
+        },
+        { // WEB CLIPPING
+          id: 321,
+          type: 'embed',
+          html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/p78q5T2fkcY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+          caption: `Freshman Congresswoman Alexandria Ocasio-Cortez taking to Twitter to criticize House Speaker Nancy Pelosi`,
+          startTime: 122459
+        }
+      ],
+      credits: `
+        ...
+      `,
+      transcript: `
+        ...
+      `
+    },
+    { // EPISODE
+      id: 0,
+      soundcloudId: 1234,
+      runtime: "1 hour, 7 minutes",
+      duration: 360000,
+      datePosted: "2019-12-11T08:00:00.000Z",
+      title: 'Episode 81: Animal Rights as Media and Pop Culture Punchline',
+      coverImage: 'https://cdn1.player.fm/images/14302089/series/sPFCeeGeQ1hLU73v/512.png',
+      contentWarning: 'violence',
+      description: `
+        In countless pop culture and media depictions, animal rights advocates and vegetarians in general, are viewed as effete weirdos, dirty hippies and humorless busybodies. Pop culture staples from "South Park" to "How I Met Your Mother" to "Six Feet Under" have used animal rights and those concerned for animal welfare as a go-to, faux populist target.
+
+        Content-wise, mocking vegans is the lowest hanging fruit. They’re difficult and self-righteous, a ready-made punching bag. Additionally, the press––including leading left-of-center media MSNBC, The Nation, and Jacobin––ignore the issue entirely.
+
+        But what if the subject is worth a second look? And, what if our general cultural dislike of vegans is based not on objective experience but a cheap stereotype that allows for in-group signaling, permitting us, above all, to not ask or answer uncomfortable questions about where animals fit on the left.
+
+        We are joined by author and professor Dr. Lori Gruen and decolonial theorist Aph Ko.
       `,
       content: [
         { // IMAGE
